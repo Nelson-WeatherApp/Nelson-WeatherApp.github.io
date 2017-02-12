49df44ee984057eb9998ddaabf2f7f921ab04840
Alex Nelson <caminelson.cn@gmail.com>
2017-02-12 01:26:21 -0800

    view setup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,6 @@
 class UserController < ApplicationController
   def index
+    @user=User.find(1)
+    @location = Location.first
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,3 @@
 class Location < ApplicationRecord
+  serialize :wardorbe
 end
